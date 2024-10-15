481e8fb913d62bf38b3cf9d3e4ffb1fbc1a0b57d
Park EunSol <eunsolguni1353@gmail.com>
2024-10-15 11:14:43 +0900

    fifth

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 public static void run() {
-System.out.println("hello world");
-}
+	public static void run() {
+		System.out.println("hello world");
+		System.out.println("홍길동");
+		}
 }
